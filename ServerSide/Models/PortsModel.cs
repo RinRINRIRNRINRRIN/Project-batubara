8b2d8c61ffb9d836a029307d90f20b8b4d51e289
Karin <karinking9078loop@gmail.com>
2025-12-08 11:44:47 +0700

    Add model ports

---
+++ ServerSide/Models/PortsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSide.Models
+{
+    public class PortsModel
+    {
+        public string DeviceNames { get; set; }
+        public string DevicePort { get; set; }
+    }
+}
